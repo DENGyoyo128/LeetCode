9e4b2c17c4ab4f3976a767ebd7e80793f596b7d0
DENGyoyo128 <deng01128@outlook.com>
2025-09-05 20:34:13 +0800

    LC-linked-list-cycle [Runtime: 56 ms; Memory: 19.7 MB]

---
+++ linked-list/linked-list-cycle.py
@@ -0,0 +1,21 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head or not head.next:
+            return False
+        
+        slow = head  # 慢指针，每次走一步
+        fast = head  # 快指针，每次走两步
+        
+        while fast and fast.next:
+            slow = slow.next        # 慢指针走一步
+            fast = fast.next.next   # 快指针走两步
+            
+            if slow == fast:        # 如果相遇，说明有环
+                return True
+        
+        return False  # 快指针到达末尾，说明无环
