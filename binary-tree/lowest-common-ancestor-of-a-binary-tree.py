58b63646c19b32d534e7b1e2381e9dd1ab9b564c
DENGyoyo128 <deng01128@outlook.com>
2025-09-14 16:06:15 +0800

    LC-lowest-common-ancestor-of-a-binary-tree [Runtime: 47 ms; Memory: 22.2 MB]

---
+++ binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == q or root == p or root is None:
+            return root
+# 那么先用left和right接住左子树和右子树的返回值
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left is not None and right is not None:
+            return root
+
+        if left is None and right is not None:
+            return right
+        elif left is not None and right is None:
+            return left
+        else: 
+            return None
+
