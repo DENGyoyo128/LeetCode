f6a76364f0e6831d6e501687175202aa4b8c88df
DENGyoyo128 <deng01128@outlook.com>
2025-11-06 16:53:13 +0800

    LC-project-employees-i [Runtime: 1652 ms; Memory: 0B]

--- database/project-employees-i.sql
+++ database/project-employees-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select p.project_id,avg(experience_years) as average_years
+select p.project_id,round(avg(experience_years),2) as average_years
 from project p
 left join employee e on e.employee_id=p.employee_id
 group by p.project_id
