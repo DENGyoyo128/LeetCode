a86ae294a778980409ada9e8a2294394580bc0c5
DENGyoyo128 <deng01128@outlook.com>
2025-11-07 15:37:34 +0800

    LC-percentage-of-users-attended-a-contest [Runtime: 966 ms; Memory: 0B]

--- database/percentage-of-users-attended-a-contest.sql
+++ database/percentage-of-users-attended-a-contest.sql
@@ -3,4 +3,4 @@ select r.contest_id,round(100*count(distinct r.user_id)/tu.total_users,2) as per
 from register r
 CROSS JOIN (SELECT COUNT(*) AS total_users FROM Users) AS tu
 group by r.contest_id
-order by percentage desc
+order by percentage desc,r.contest_id ASC
