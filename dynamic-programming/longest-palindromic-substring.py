7cdc604e8f2d1833e69046862cc3eff2a453d960
DENGyoyo128 <deng01128@outlook.com>
2025-11-07 19:49:10 +0800

    LC-longest-palindromic-substring [Runtime: 1485 ms; Memory: 25.6 MB]

--- dynamic-programming/longest-palindromic-substring.py
+++ dynamic-programming/longest-palindromic-substring.py
@@ -4,27 +4,15 @@ class Solution:
         if n < 2:
             return s
 
-        # dp[i][j]：s[i..j] 是否是回文子串
         dp = [[False] * n for _ in range(n)]
-
         start, maxlen = 0, 1
-        # 长度为 1
-        for i in range(n):
-            dp[i][i] = True
 
-        # 长度为 2
-        for i in range(n - 1):
-            if s[i] == s[i + 1]:
-                dp[i][i + 1] = True
-                start, maxlen = i, 2
-
-        # 长度 >= 3
-        for L in range(3, n + 1):                   # 子串长度
-            for i in range(0, n - L + 1):
-                j = i + L - 1
-                if s[i] == s[j] and dp[i + 1][j - 1]:
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = True  # 单字符
+            for j in range(i + 1, n):
+                if s[i] == s[j] and (j - i <= 1 or dp[i + 1][j - 1]):
                     dp[i][j] = True
-                    if L > maxlen:
-                        start, maxlen = i, L
+                    if j - i + 1 > maxlen:
+                        start, maxlen = i, j - i + 1
 
         return s[start:start + maxlen]
