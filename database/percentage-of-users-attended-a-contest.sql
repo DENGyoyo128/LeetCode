b9ee1839c33f9699545a1b3e16a9e3732987ddc8
DENGyoyo128 <deng01128@outlook.com>
2025-11-07 15:35:08 +0800

    LC-percentage-of-users-attended-a-contest [Runtime: N/A; Memory: N/A]

---
+++ database/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select r.contest_id,round(100*count(distinct r.user_id)/tu.total_users,2) as percentage 
+from register r
+CROSS JOIN (SELECT COUNT(*) AS total_users FROM Users) AS tu
+group by r.contest_id
+order by percentage desc
