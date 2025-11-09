c39c60b502784b020a083c753558e4bb4bf37849
DENGyoyo128 <deng01128@outlook.com>
2025-11-09 12:41:47 +0800

    LC-patients-with-a-condition [Runtime: 369 ms; Memory: 0B]

--- database/patients-with-a-condition.sql
+++ database/patients-with-a-condition.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
-select *
-from patients
-where conditions like '%DIAB1%'
+-- select *
+-- from patients
+-- where conditions like '%DIAB1%'
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%'    
+   OR conditions LIKE '% DIAB1%';
