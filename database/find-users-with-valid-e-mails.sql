92abcaee2514986c789d6b80176f10feb0d3bc3e
DENGyoyo128 <deng01128@outlook.com>
2025-11-09 15:37:25 +0800

    LC-find-users-with-valid-e-mails [Runtime: 676 ms; Memory: 0B]

--- database/find-users-with-valid-e-mails.sql
+++ database/find-users-with-valid-e-mails.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
 SELECT user_id, name, mail
 FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
+WHERE REGEXP_LIKE(
+  mail,
+  '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$',
+  'c'   -- case-sensitive
+);
