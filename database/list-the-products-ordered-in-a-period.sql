d7ce6f58c9b9c1698b3bc15e131ce357e74a3f0e
DENGyoyo128 <deng01128@outlook.com>
2025-11-09 15:20:02 +0800

    LC-list-the-products-ordered-in-a-period [Runtime: 683 ms; Memory: 0B]

--- database/list-the-products-ordered-in-a-period.sql
+++ database/list-the-products-ordered-in-a-period.sql
@@ -3,6 +3,6 @@
 select  p.product_name,sum(o.unit) as unit
 from orders o
 left join products p on p.product_id=o.product_id
-where o.order_date like "%-02-%" 
+where o.order_date like "%2020-02-%" 
 group by p.product_id
 having unit>=100
