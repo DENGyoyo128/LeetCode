6edec50eee22b3e22df81210b714d7a3404bd1fb
DENGyoyo128 <deng01128@outlook.com>
2025-11-09 12:26:26 +0800

    LC-employees-whose-manager-left-the-company [Runtime: 335 ms; Memory: 0B]

--- database/employees-whose-manager-left-the-company.sql
+++ database/employees-whose-manager-left-the-company.sql
@@ -3,7 +3,8 @@ SELECT employee_id
 FROM employees
 WHERE salary < 30000
   AND manager_id IS NOT NULL
-  AND manager_id NOT IN (SELECT employee_id FROM employees);
+  AND manager_id NOT IN (SELECT employee_id FROM employees)
+order by employee_Id asc
 
 
 
