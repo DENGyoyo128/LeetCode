21ca22a5474e3865bba43fe033ab4a22551a62ad
DENGyoyo128 <deng01128@outlook.com>
2025-11-09 21:56:25 +0800

    LC-top-k-frequent-elements [Runtime: 3 ms; Memory: 21.1 MB]

---
+++ heap-priority-queue/top-k-frequent-elements.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 使用字典统计数字出现次数
+        time_dict = defaultdict(int)
+        for num in nums:
+            time_dict[num] += 1
+        # 更改字典，key为出现次数，value为相应的数字的集合
+        index_dict = defaultdict(list)
+        for key in time_dict:
+            index_dict[time_dict[key]].append(key)
+        # 排序
+        key = list(index_dict.keys())
+        key.sort()
+        result = []
+        cnt = 0
+        # 获取前k项
+        while key and cnt != k:
+            result += index_dict[key[-1]]
+            cnt += len(index_dict[key[-1]])
+            key.pop()
+
+        return result[0: k]
