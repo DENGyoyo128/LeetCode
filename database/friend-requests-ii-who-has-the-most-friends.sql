5afdb5272124881b5a4d9dc2ba62b7fb4a004b31
DENGyoyo128 <deng01128@outlook.com>
2025-11-11 19:42:09 +0800

    LC-friend-requests-ii-who-has-the-most-friends [Runtime: 297 ms; Memory: 0B]

--- database/friend-requests-ii-who-has-the-most-friends.sql
+++ database/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,18 +1,17 @@
 # Write your MySQL query statement below
-with accept as
-(select accepter_id as id,count(accepter_id) as num
+with all_num as
+((select accepter_id as id,count(accepter_id) as num
 from requestaccepted
 group by accepter_id
-order by num desc),
-request as
+order by num desc)
+union all
 (select requester_id as id,count(requester_id) as num
 from requestaccepted
-group by accepter_id
-order by num desc
-)
-select a.id ,a.num+r.num as num
-from request r
-join accept a on a.id=r.id
+group by requester_id
+order by num desc))
+-- select id ,a.num+r.num as num
+select id, sum(num) as num
+from all_num
+group by id 
 order by num desc
 limit 1
-
