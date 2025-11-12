0b1c950d28e526588d38568c637f14b7c51159d3
DENGyoyo128 <deng01128@outlook.com>
2025-11-12 11:17:25 +0800

    LC-find-users-with-valid-e-mails [Runtime: 301 ms; Memory: 67.5 MB]

--- database/find-users-with-valid-e-mails.py
+++ database/find-users-with-valid-e-mails.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
+# def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+#     pattern = r'^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
+#     mask = users['mail'].str.match(pattern, na=False)
+#     valid = users[mask]
+#     return valid
+
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    pattern = r'^[A-Za-z][A-Za-z0-9_.-]*@leetcode\.com$'
-    mask = users['mail'].str.match(pattern, na=False)
-    valid = users[mask]
-    return valid
+    
+    is_valid = lambda x: set(x).issubset(valid_chars)
+    letters = set(ascii_letters)
+    valid_chars = letters | set(digits) | {'_', '.', '-'}
+
+    return users[(users.mail.str[0].isin(letters)) &         # <--1)
+                 (users.mail.str[~12:] == '@leetcode.com') & # <--2)
+                 (users.mail.str[:~12].apply(is_valid))]     # <--3)
+
