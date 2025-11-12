e2e98606222bd66fbd2de2099ce70a2f8e9639de
DENGyoyo128 <deng01128@outlook.com>
2025-11-12 14:44:44 +0800

    LC-nth-highest-salary [Runtime: 305 ms; Memory: 66.8 MB]

--- database/nth-highest-salary.py
+++ database/nth-highest-salary.py
@@ -3,7 +3,7 @@ import pandas as pd
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
     # 取不同工资，按降序
     s = employee['salary'].drop_duplicates().sort_values(ascending=False)
-    val = s.iloc[N-1] if len(s) >= N else np.nan  # 不足 n 个则返回 NaN (等价于 SQL 的 NULL)
+    val = s.iloc[N-1] if len(s) >= N and N>0 else np.nan  # 不足 n 个则返回 NaN (等价于 SQL 的 NULL)
     return pd.DataFrame({f'getNthHighestSalary({N})': [val]})
 
     # s = employee['salary'].drop_duplicates().nlargest(n)
