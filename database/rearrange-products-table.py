64011c0edcc5d8fba63f8df2d04ba626ef411f39
DENGyoyo128 <deng01128@outlook.com>
2025-11-12 15:40:42 +0800

    LC-rearrange-products-table [Runtime: 319 ms; Memory: 67.9 MB]

---
+++ database/rearrange-products-table.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    out = pd.melt(
+        products,
+        id_vars=["product_id"],
+        value_vars=["store1", "store2", "store3"],
+        var_name="store",
+        value_name="price",
+    )
+    out = out.dropna(subset=["price"]).reset_index(drop=True)
+    return out[["product_id", "store", "price"]]
