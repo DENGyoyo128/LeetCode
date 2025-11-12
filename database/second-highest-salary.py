e8d8602a8dfae237ed532a92a08216a3dcffadb2
DENGyoyo128 <deng01128@outlook.com>
2025-11-12 14:59:21 +0800

    LC-second-highest-salary [Runtime: N/A; Memory: N/A]

--- database/second-highest-salary.py
+++ database/second-highest-salary.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
-    employee.sort_values('salary').drop_duplicates()
+    employee.sort_values('salary').drop_duplicates(subset=['salary'])
     second=employee.iloc[1:2]
     out=second[['salary']].rename(columns={'salary':'SecondHighestSalary'})
-    if out.empty  or len(employee)==1:
+    if out.empty :
         out = pd.DataFrame({'SecondHighestSalary': [None]})
+
     return out[['SecondHighestSalary']]
     # s = employee['salary'].drop_duplicates().nlargest(2)
     # val = s.iloc[-1] if len(s) == 2 else None  # 不足两种工资 -> None(显示为 NaN)
