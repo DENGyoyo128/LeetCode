d51f524541e57cb6e903df26917cfedba4d52413
DENGyoyo128 <deng01128@outlook.com>
2025-11-13 22:08:47 +0800

    LC-decode-string [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ recursion/decode-string.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        num = 0
+        cur = []
+        st = []  # 栈里放 (之前的字符串list, k)
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)       # 处理多位数
+            elif ch == '[':
+                st.append((cur, num))          # 入栈保存现场
+                cur, num = [], 0               # 开启新一层
+            elif ch == ']':
+                prev, k = st.pop()             # 出栈恢复现场
+                cur = prev + cur * k           # 当前解码重复并拼回
+            else:
+                cur.append(ch)                 # 普通字母
+        return ''.join(cur)
+        
