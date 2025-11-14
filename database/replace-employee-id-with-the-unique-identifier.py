69296115e4158ceb9e020816c2a4f2216a535e5a
DENGyoyo128 <deng01128@outlook.com>
2025-11-14 13:37:15 +0800

    LC-replace-employee-id-with-the-unique-identifier [Runtime: 363 ms; Memory: 68.2 MB]

---
+++ database/replace-employee-id-with-the-unique-identifier.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    employees=employees.merge(employee_uni,on="id",how="left")
+    employees=employees[["unique_id","name"]]
+    return employees
