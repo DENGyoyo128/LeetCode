e66083a94c7bed2e7b1d2cfe1e966c0efeb79423
DENGyoyo128 <deng01128@outlook.com>
2025-11-19 01:12:34 +0800

    LC-longest-consecutive-sequence [Runtime: 53 ms; Memory: 33.2 MB]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -3,14 +3,13 @@ class Solution:
         if not nums:
             return 0
         nums_set=set(nums)
-        conse_length=1
         length=1
         for num in nums_set:
             if num-1 not in nums_set:
-                num_start=1
+                num_start=num
                 cur_length=1
-                while num+1 in nums_set:
-                    num+=1
-                    conse_length+=1
-                length=max(cur_length,conse_length)
+                while num_start+1 in nums_set:
+                    num_start+=1
+                    cur_length+=1
+                length=max(length,cur_length)
         return length
