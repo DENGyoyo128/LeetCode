f93e5e4a8a5c54015de961f8df4e9816f141f36b
DENGyoyo128 <deng01128@outlook.com>
2025-11-18 20:54:22 +0800

    LC-group-anagrams [Runtime: 15 ms; Memory: 20.8 MB]

--- sorting/group-anagrams.py
+++ sorting/group-anagrams.py
@@ -1,8 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # record={}
+        # path=[]
+        # result=[]
+        # for st in  strs:
+        #     for s in st:
+        #         if s in record:
+        #             record[s]-=1
+        #         record[s]+=1
+        #     #如果三个都能找到
+        #     path.append(str)
 
-        mp = collections.defaultdict(list)
-        for st in strs:
-            key = "".join(sorted(st))
-            mp[key].append(st)
-        return list(mp.values())
+        groups=defaultdict(list)
+        for str in strs:
+            key="".join(sorted(str))
+            groups[key].append(str)
+        return list(groups.values())
+            
+            
