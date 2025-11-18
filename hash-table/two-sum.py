9859e9333a308f51c37375820cfa18f4420945fb
DENGyoyo128 <deng01128@outlook.com>
2025-11-18 19:30:00 +0800

    LC-two-sum [Runtime: 0 ms; Memory: 18.9 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        records=dict()
-        for index,value in enumerate(nums):
-            if target-value in records:
-                return [records[target-value],index]
-            records[value]=index
-        return[]
+        result={}
+        for index, value in enumerate(nums):
+            if target-value in  result:
+                return [result[target-value],index]
+            result[value]=index
+        return []
