6d586f426fe288712f84f83ae18c4a15b1163049
DENGyoyo128 <deng01128@outlook.com>
2025-11-19 01:06:50 +0800

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- hash-table/longest-consecutive-sequence.py
+++ hash-table/longest-consecutive-sequence.py
@@ -1,17 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        longest_streak = 0
-        num_set = set(nums)
-
-        for num in num_set:
-            if num - 1 not in num_set:
-                current_num = num
-                current_streak = 1
-
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_streak += 1
-
-                longest_streak = max(longest_streak, current_streak)
-
-        return longest_streak
+        nums_set=set(nums)
+        conse_length=1
+        for num in nums_set:
+            if num-1 not in nums_set:
+                num_start=1
+                cur_length=1
+                while num+1 in nums_set:
+                    num+=1
+                    conse_length+=1
+                length=max(cur_length,conse_length)
+        return length
