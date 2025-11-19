8e7e3ae21640134ef3b6e73229ef5f782131df73
DENGyoyo128 <deng01128@outlook.com>
2025-11-19 18:04:05 +0800

    LC-move-zeroes [Runtime: 7 ms; Memory: 18.8 MB]

--- two-pointers/move-zeroes.py
+++ two-pointers/move-zeroes.py
@@ -3,13 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        fast=0
-        slow = 0
-        for fast in range(len(nums)):
-            if nums[fast] != 0:
-                nums[slow] = nums[fast]
-                slow += 1
-        # Fill the remaining elements with zeros
-        for i in range(slow, len(nums)):
-            nums[i] = 0
+        # left=0
+        # right=len(nums)-1
+        # while left<right:
+        #     if nums[left]=0:
+        #         nums[left],nums[right]=nums[right],nums[left]
+        # 1) 先把所有非零元素“稳定地挤到前面”
+        # 2) 剩下的位置全部填 0
+        slow=0
+        n=len(nums)
+        for fast in range(n):
+            if nums[fast]!=0:
+                nums[slow]=nums[fast]
+                slow+=1
+        while slow < n:
+            nums[slow]=0
+            slow+=1
