c5965f0097104a9cea7325bc045733f440c43578
DENGyoyo128 <deng01128@outlook.com>
2025-11-19 16:56:13 +0800

    LC-container-with-most-water [Runtime: N/A; Memory: N/A]

--- two-pointers/container-with-most-water.py
+++ two-pointers/container-with-most-water.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        ans=0
         left=0
-        right=len(height)-1
-        #这两个指针还没有相遇，还可以构成面积
+        right=len(height)
+        ans=0
         while left<right:
-            area=(right-left)*min(height[left],height[right])
+            area=min(height[left],height[right])*(right-left)
             ans=max(ans,area)
-            #如果说左边矮，就把左边去掉，右边矮，就把右边去掉
-            if height[left]<height[right]:
-                left+=1
-            else:
-                right-=1
         return ans
+            
+        
