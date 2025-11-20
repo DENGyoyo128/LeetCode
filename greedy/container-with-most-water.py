719f62b76144eb1b6a091a6e176d509dc2f52e53
DENGyoyo128 <deng01128@outlook.com>
2025-11-20 19:27:03 +0800

    LC-container-with-most-water [Runtime: 103 ms; Memory: 28.4 MB]

--- greedy/container-with-most-water.py
+++ greedy/container-with-most-water.py
@@ -1,30 +1,17 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # left=0
-        # right=len(height)-1
-        # ans=0
-        # while left<right:
-        #     area=min(height[left],height[right])*(right-left)
-        #     ans=max(ans,area)
-        #     left+=1
-        #     right-=1
-        # return ans
+        left=0
+        right=len(height)-1
+        ans=0
+        area=0
+        while left< right:
+            area=min(height[left],height[right])*(right-left)
             
-#“面积由短板决定 → 谁短就动谁”，不要两个指针一起往里缩。
-
-        left = 0
-        right = len(height) - 1
-        ans = 0
-
-        while left < right:
-            h = min(height[left], height[right])
-            area = h * (right - left)
-            ans = max(ans, area)
-
-            # 只移动短的那一侧
-            if height[left] < height[right]:
-                left += 1
+            ans=max(ans,area)
+            
+            if height[left]<height[right]:
+                left+=1
             else:
-                right -= 1
+                right-=1
 
         return ans
