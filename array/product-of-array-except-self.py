a0185d45e3d108786852bcc6bee6d52b6970368e
DENGyoyo128 <deng01128@outlook.com>
2025-11-20 21:11:41 +0800

    LC-product-of-array-except-self [Runtime: 28 ms; Memory: 23.3 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -1,43 +1,19 @@
-# class Solution:
-#     def productExceptSelf(self, nums: List[int]) -> List[int]:
-#         n = len(nums)
-#         ans = [1] * n
-        
-#         # 先计算每个元素左边的乘积，并存入ans
-#         left = 1
-#         for i in range(n):
-#             ans[i] = left
-#             left *= nums[i]
-        
-#         # 再计算每个元素右边的乘积，并乘以ans中已有的左边乘积
-#         right = 1
-#         for i in range(n-1, -1, -1):
-#             ans[i] *= right
-#             right *= nums[i]
-        
-#         return ans
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        answer = [1] * n
-        left, right = 0, n - 1
-        lp, rp = 1, 1  # lp: 左乘积累积, rp: 右乘积累积
-        
-        while left < n and right >= 0:
-            # 更新左右指针位置的答案
-            answer[left] *= lp
-            answer[right] *= rp
+        n=len(nums)
+        ans=[1]*n
+        left=0
+        right=len(nums)-1
+        rp=1
+        lp=1
+        while left<n and right>=0:
+            ans[left]*=lp
+            ans[right]*=rp
             
-            # 更新左右乘积累积
-            lp *= nums[left]
-            rp *= nums[right]
-            
-            # 移动指针
-            left += 1
-            right -= 1
-        
-        return answer
+            lp*=nums[left]
+            rp*=nums[right]
 
-        
+            left+=1
+            right-=1
+            
+        return ans
