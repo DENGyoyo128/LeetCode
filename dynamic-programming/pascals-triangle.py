4c5c09660d9da93ee008938a320c7e42c6c8652e
DENGyoyo128 <deng01128@outlook.com>
2025-11-26 20:41:01 +0800

    LC-pascals-triangle [Runtime: 2 ms; Memory: 17.6 MB]

--- dynamic-programming/pascals-triangle.py
+++ dynamic-programming/pascals-triangle.py
@@ -1,9 +1,9 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
+        res=[]
         for i in range(numRows):
-            row = [1] * (i + 1)
-            for j in range(1, i):
-                row[j] = res[i - 1][j - 1] + res[i - 1][j]
+            row=[1]*(i+1)
+            for j in range(1,i):
+                row[j]=res[i-1][j]+res[i-1][j-1]
             res.append(row)
         return res
