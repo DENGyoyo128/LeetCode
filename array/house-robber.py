687f82b48d3b631c2af5c5226f179a3ac6ffc16c
DENGyoyo128 <deng01128@outlook.com>
2025-11-26 21:30:51 +0800

    LC-house-robber [Runtime: 0 ms; Memory: 17.7 MB]

--- array/house-robber.py
+++ array/house-robber.py
@@ -3,8 +3,8 @@ class Solution:
         n=len(nums)
         dp=[0]*n
         dp[0]=ans=nums[0]
+        
         for i in range(1,n):
-            a=dp[i-2]
-            dp[i]=max(dp[i-1],a+nums[i])
+            dp[i]=max(dp[i-2]+nums[i],dp[i-1])
             ans=max(ans,dp[i])
         return ans
