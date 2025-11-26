ba6adab0dc4b154590ea2e0a5806a76fc977d6ea
DENGyoyo128 <deng01128@outlook.com>
2025-11-26 19:26:53 +0800

    LC-edit-distance [Runtime: 71 ms; Memory: 21.2 MB]

--- string/edit-distance.py
+++ string/edit-distance.py
@@ -1,21 +1,22 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        dp = [[0]*(n+1) for _ in range(m+1)]
+        #dp数组的含义dp[i][j]表示长度i（下标i-1为结尾）的word1和长度j（下标j-1位结尾）的word2的最少操作次数
+        m=len(word1)
+        n=len(word2)
 
+        dp=[[0]*(n+1) for _ in range(m+1)]
         for i in range(m+1):
-            dp[i][0] = i
+            dp[i][0]=i
         for j in range(n+1):
-            dp[0][j] = j
+            dp[0][j]=j
 
-        for i in range(1, m+1):
-            for j in range(1, n+1):
-                if word1[i-1] == word2[j-1]:
-                    dp[i][j] = dp[i-1][j-1]
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                if word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
                 else:
-                    dp[i][j] = min(
-                        dp[i-1][j] + 1,    # 删除 word1[i-1]
-                        dp[i][j-1] + 1,    # 插入 word2[j-1]
-                        dp[i-1][j-1] + 1   # 替换为 word2[j-1]
-                    )
-        return dp[-1][-1]
+                    dp[i][j]=min(
+                        dp[i-1][j]+1,
+                        dp[i][j-1]+1,
+                        dp[i-1][j-1]+1)
+        return dp[m][n]
