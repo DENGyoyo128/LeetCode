0b29923936ebc6c354cfa5cb0f8d676cf12b8882
DENGyoyo128 <deng01128@outlook.com>
2025-11-26 21:13:39 +0800

    LC-maximum-product-subarray [Runtime: 9 ms; Memory: 19.5 MB]

--- dynamic-programming/maximum-product-subarray.py
+++ dynamic-programming/maximum-product-subarray.py
@@ -8,6 +8,7 @@ class Solution:
         for i in range(1,n):
             a=dp_max[i-1]*nums[i]
             b=dp_min[i-1]*nums[i]
-            res=max(a,b)
-            ans=max(ans,res)
+            dp_max[i]=max(nums[i],a,b)
+            dp_min[i]=min(nums[i],a,b)
+            ans=max(ans,dp_max[i])
         return ans
