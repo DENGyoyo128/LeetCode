9d4b1d822ccefc2ec224b05a8abd359d507574ec
DENGyoyo128 <deng01128@outlook.com>
2025-11-29 13:48:01 +0800

    LC-permutations [Runtime: 0 ms; Memory: 18.2 MB]

--- backtracking/permutations.py
+++ backtracking/permutations.py
@@ -1,19 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        n = len(nums) 
-        res, path = [], [] 
-        used = [False] * n 
-        def dfs(): 
-            if len(path) == n: 
-                res.append(path.copy()) 
+        res=[]
+        path=[]
+        n=len(nums)
+        used=[False]*n
+        def dfs():
+            if len(path)==len(nums):
+                res.append(path.copy())
                 return
             for i in range(n):
-                if used[i]: 
-                    continue 
-                used[i] = True 
-                path.append(nums[i]) 
-                dfs() 
-                path.pop() 
-                used[i] = False
-        dfs() 
+                num=nums[i]
+                if used[i]:
+                    continue
+                used[i]=True
+                path.append(nums[i])
+                dfs()
+                path.pop()
+                used[i]=False
+        dfs()
         return res
+
+
+
+
