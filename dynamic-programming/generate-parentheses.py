3f1b53e441e5fe623f1d04e5ce0eabb79d642b53
DENGyoyo128 <deng01128@outlook.com>
2025-11-29 12:00:36 +0800

    LC-generate-parentheses [Runtime: 0 ms; Memory: 18.2 MB]

--- dynamic-programming/generate-parentheses.py
+++ dynamic-programming/generate-parentheses.py
@@ -1,14 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        #哇，好久不见
-        res = []
-        def dfs(cur: str, left: int, right: int):
-            if len(cur) == 2 * n:
-                res.append(cur)
+        res=[]
+        def backtrack(path:str,left_cnt:int,right_cnt:int):
+            if len(path)==2*n:
+                res.append(path)
                 return
-            if left < n:
-                dfs(cur + "(", left + 1, right)
-            if right < left:
-                dfs(cur + ")", left, right + 1)
-        dfs("", 0, 0)
+            
+            if left_cnt<n:
+                backtrack(path+"(",left_cnt+1,right_cnt)
+            if right_cnt<left_cnt:
+                backtrack(path+")",left_cnt,right_cnt+1)
+            
+        backtrack("",0,0)
         return res
