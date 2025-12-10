852d2adf36fa6a4f6fae559c3f9b308788597596
DENGyoyo128 <deng01128@outlook.com>
2025-12-10 21:19:13 +0800

    LC-validate-binary-search-tree [Runtime: N/A; Memory: N/A]

--- binary-search-tree/validate-binary-search-tree.py
+++ binary-search-tree/validate-binary-search-tree.py
@@ -5,19 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.maxVal = float('-inf')  # 因为后台测试数据中有int最小值
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        res=[]
+        def dfs(node):
+            if node is None:
+                return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
 
-    def isValidBST(self, root):
-        if root is None:
-            return True
-
-        left = self.isValidBST(root.left)
-        # 中序遍历，验证遍历的元素是不是从小到大
-        if self.maxVal < root.val:
-            self.maxVal = root.val
-        else:
-            return False
-        right = self.isValidBST(root.right)
-
-        return left and right
+        for i in range(1,len(res)):
+            if res[i]<res[i-1]:
+                return False
+        
+        return True
+        
