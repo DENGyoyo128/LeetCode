71991a96fc04c8d2a8f3e76671ade3ff35938174
DENGyoyo128 <deng01128@outlook.com>
2025-12-11 02:31:22 +0800

    LC-binary-tree-level-order-traversal [Runtime: 0 ms; Memory: 18.5 MB]

--- binary-tree/binary-tree-level-order-traversal.py
+++ binary-tree/binary-tree-level-order-traversal.py
@@ -6,24 +6,24 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res=[]
         if not root:
-            return []
-#定义一个队列，如果root不为空，加入第一个节点
-        queue = collections.deque([root])
-        result = []
-#当队列不为空，一直去遍历
-        while queue:
-#定义一维数组（当前层），一维数组循环结束之后，放入二维数组（最终结果集）
-            level = []
-#通过size控制本层的元素
-            for _ in range(len(queue)):
-#弹出元素，记录在一维数组里	
-                cur = queue.popleft()
-                level.append(cur.val)
-#进入下一层队列里
-                if cur.left:
-                    queue.append(cur.left)
-                if cur.right:
-                    queue.append(cur.right)
-            result.append(level)
-        return result
+            return res  
+        q=deque()
+        q.append(root)
+        while q:
+            size=len(q)
+            level=[]
+            for _ in range(size):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            res.append(level)
+        return res
+                    
+
+
+        
