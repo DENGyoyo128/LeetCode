714029b361056e60d16dcf0b974a8d44ceafeaff
DENGyoyo128 <deng01128@outlook.com>
2025-12-26 06:47:27 +0800

    LC-partition-labels [Runtime: 7 ms; Memory: 17.2 MB]

--- two-pointers/partition-labels.py
+++ two-pointers/partition-labels.py
@@ -1,16 +1,15 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_occurrence = {}  # 存储每个字符最后出现的位置
-        for i, ch in enumerate(s):
-            last_occurrence[ch] = i
-
-        result = []
-        start = 0
-        end = 0
-        for i, ch in enumerate(s):
-            end = max(end, last_occurrence[ch])  # 找到当前字符出现的最远位置
-            if i == end:  # 如果当前位置是最远位置，表示可以分割出一个区间
-                result.append(end - start + 1)
-                start = i + 1
-
-        return result
+        last_occur={}
+        for i,ch in enumerate(s):
+            last_occur[ch]=i
+        
+        start=0
+        end=0
+        res=[]
+        for i,ch in enumerate(s):
+            end=max(end,last_occur[ch])
+            if i==end:
+                res.append(end-start+1)
+                start=i+1
+        return res
