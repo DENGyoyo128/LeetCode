38e750a4edebbda1ce6660a809a2f4ef0452e31f
DENGyoyo128 <deng01128@outlook.com>
2025-12-30 05:08:52 +0800

    LC-path-sum-iii [Runtime: 0 ms; Memory: 17.8 MB]

--- depth-first-search/path-sum-iii.py
+++ depth-first-search/path-sum-iii.py
@@ -6,26 +6,24 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        prefix = defaultdict(int)
-        prefix[0] = 1   # 空路径和为 0 出现一次
-        self.ans = 0
+        cnt = defaultdict(int)
+        cnt[0] = 1  # 空前缀：方便处理“从根开始”的路径
 
         def dfs(node, cur_sum):
             if not node:
-                return
-
+                return 0
             cur_sum += node.val
-            # 以当前节点为终点、和为 targetSum 的路径条数
-            self.ans += prefix[cur_sum - targetSum]
+   # 以当前节点为结尾的、满足 targetSum 的路径条数
+            res = cnt[cur_sum - targetSum]
 
-            # 当前前缀和计数 +1
-            prefix[cur_sum] += 1
+            # 记录当前前缀和
+            cnt[cur_sum] += 1
 
-            dfs(node.left, cur_sum)
-            dfs(node.right, cur_sum)
+            # 继续往下
+            res += dfs(node.left, cur_sum)
+            res += dfs(node.right, cur_sum)
 
-            # 回溯：离开该节点，前缀和计数 -1
-            prefix[cur_sum] -= 1
-
-        dfs(root, 0)
-        return self.ans
+            # 回溯：撤销当前节点的贡献
+            cnt[cur_sum] -= 1
+            return res
+        return dfs(root, 0)
